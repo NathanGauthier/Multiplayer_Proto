ebcd97e0a22331ab6eb4978f69894e53eb31e50e
Ngauthier <ngauthier@artfx.fr>
2023-02-17 18:01:22 +0100

    multiSelection

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class UnitSelectionHandler : MonoBehaviour
 {
 
+    [SerializeField] private RectTransform unitSelectionArea = null;
     [SerializeField] private LayerMask layerMask = new LayerMask();
+
+    private Vector2 startPosition;
+
+    private RTSPlayer player;
     private Camera mainCamera;
 
     public  List<Unit> selectedUnits {get;} = new List<Unit>();
@@ -19,39 +25,93 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private void Update() 
     {
+
+        if(player == null)
+        {
+            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        }
+
         if(Mouse.current.leftButton.wasPressedThisFrame)
         {
-             foreach(Unit selectedUnit in selectedUnits)
-            {
-            selectedUnit.Deselect();
-            }
-            selectedUnits.Clear();
+             StarSelectionArea();
         }
         else if(Mouse.current.leftButton.wasReleasedThisFrame)
         {
             ClearSelectionArea();
         }
 
+        else if(Mouse.current.leftButton.isPressed)
+        {
+            UpdateSelectionArea();
+        }
+    }
+
+    private void StarSelectionArea()
+    {
+        foreach(Unit selectedUnit in selectedUnits)
+            {
+            selectedUnit.Deselect();
+            }
+            selectedUnits.Clear();
+
+            unitSelectionArea.gameObject.SetActive(true);
+
+            startPosition = Mouse.current.position.ReadValue();
+
+            UpdateSelectionArea();
+    }
+
+    private void UpdateSelectionArea()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        float areaWidth = mousePosition.x - startPosition.x;
+        float areaHeight = mousePosition.y - startPosition.y;
+
+        unitSelectionArea.sizeDelta =new Vector2(Mathf.Abs(areaWidth), Mathf.Abs(areaHeight)) ;
+        unitSelectionArea.anchoredPosition = startPosition + new Vector2(areaWidth / 2 , areaHeight / 2);
+
     }
 
     private void ClearSelectionArea()
     {
        
+        unitSelectionArea.gameObject.SetActive(false);
 
-
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) return ; 
-
-        if(!hit.collider.TryGetComponent<Unit>(out Unit unit)) return ;
-
-        if(!unit.hasAuthority) return ;
-
-        selectedUnits.Add(unit);
-
-        foreach ( Unit selectedUnit in selectedUnits)
+        if(unitSelectionArea.sizeDelta.magnitude == 0)
         {
-            selectedUnit.Select();
+        
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) return ; 
+
+            if(!hit.collider.TryGetComponent<Unit>(out Unit unit)) return ;
+
+            if(!unit.hasAuthority) return ;
+
+            selectedUnits.Add(unit);
+
+            foreach ( Unit selectedUnit in selectedUnits)
+            {
+                selectedUnit.Select();
+            }
+
+            return ;
+        } 
+
+        Vector2 min = unitSelectionArea.anchoredPosition - (unitSelectionArea.sizeDelta / 2);
+        Vector2 max = unitSelectionArea.anchoredPosition + (unitSelectionArea.sizeDelta / 2);
+        foreach(Unit unit in player.GetMyUnits())
+        {
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(unit.transform.position);
+            if(screenPosition.x > min.x &&
+              screenPosition.x < max.x &&
+              screenPosition.y > min.y &&
+              screenPosition.y < max.y )
+            {
+                selectedUnits.Add(unit);
+                unit.Select();
+            }
         }
 
     }
